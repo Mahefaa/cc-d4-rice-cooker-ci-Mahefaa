4f946ac9dba1fbda3d42d94974af80aacdf16a8e
Mahefaa <hei.mahefa@gmail.com>
2023-12-18 16:52:12 +0300

    chore: lint rice_cooker.ts test

--- __tests__/rice_cooker.ts
+++ __tests__/rice_cooker.ts
@@ -1,38 +1,38 @@
 import '@jest/globals'
-import {describe} from "node:test";
-import {RiceCooker} from "../core/RiceCooker";
+import { describe } from 'node:test'
+import { RiceCooker } from '../core/RiceCooker'
 
-describe('Test rice cooker', () => {
-    test('add_water ok', () => {
-        const cooker = new RiceCooker();
-        expect(cooker.water_level === 0);
+void describe('Test rice cooker', () => {
+  test('add_water ok', () => {
+    const cooker = new RiceCooker()
+    expect(cooker.water_level === 0)
 
-        cooker.add_water();
-        expect(cooker.water_level === 1);
-    })
-    test('decrease_water_level ok', () => {
-        const cooker = new RiceCooker();
-        expect(cooker.water_level === 0)
+    cooker.add_water()
+    expect(cooker.water_level === 1)
+  })
+  test('decrease_water_level ok', () => {
+    const cooker = new RiceCooker()
+    expect(cooker.water_level === 0)
 
-        cooker.add_water();
-        cooker.decrease_water_level();
+    cooker.add_water()
+    cooker.decrease_water_level()
 
-        expect(cooker.water_level === 0);
-    })
-    test('add_rice ok', () => {
-        const cooker = new RiceCooker();
-        expect(cooker.rice_amount === 0);
+    expect(cooker.water_level === 0)
+  })
+  test('add_rice ok', () => {
+    const cooker = new RiceCooker()
+    expect(cooker.rice_amount === 0)
 
-        cooker.add_rice();
+    cooker.add_rice()
 
-        expect(cooker.rice_amount === 1);
-    })
-    test('decrease_rice_amount ok', () => {
-        const cooker = new RiceCooker();
+    expect(cooker.rice_amount === 1)
+  })
+  test('decrease_rice_amount ok', () => {
+    const cooker = new RiceCooker()
 
-        cooker.add_rice();
-        cooker.decrease_rice_amount();
+    cooker.add_rice()
+    cooker.decrease_rice_amount()
 
-        expect(cooker.rice_amount === 0);
-    })
+    expect(cooker.rice_amount === 0)
+  })
 })
